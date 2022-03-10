6901ff64f3141b2d49e49d5a676439c0f7a25d35
Quang Tran <tquang1708@gmail.com>
2022-03-09 21:29:57 -0500

    added sqlx and some basic handler for sql

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
+use std::{net::SocketAddr, time::Duration};
 use axum::{
-    http::Method,
+    http::{Method, StatusCode},
     Router,
     routing::get,
+    extract::{Extension}
 };
-
-use std::net::SocketAddr;
-
 use tower_http::cors::{CorsLayer, Origin};
+use sqlx::postgres::{PgPool, PgPoolOptions};
 
 #[tokio::main]
 async fn main() {
-    // set the RUST_LOG env
+    // set the RUST_LOG env for logging
     if std::env::var_os("RUST_LOG").is_none() {
         std::env::set_var(
             "RUST_LOG",
@@ -19,12 +19,23 @@ async fn main() {
     };
     tracing_subscriber::fmt::init();
 
+    // metadata db connection
+    let db_connection_str = "postgres://postgres:password@localhost".to_string();
+    let pool = PgPoolOptions::new()
+        .max_connections(5) // move to cfg
+        .connect_timeout(Duration::from_secs(3))
+        .connect(&db_connection_str)
+        .await
+        .expect("Can't connect to database");
+
     // app routing
-    let app = Router::new().route("/api/test", get(handler)).layer(
-        CorsLayer::new()
+    let app = Router::new()
+        .route("/api/test", get(handler))
+        .route("/api/db", get(connection_pool_extractor))
+        .layer(CorsLayer::new()
             .allow_origin(Origin::exact("http://localhost:3000".parse().unwrap()))
-            .allow_methods(vec![Method::GET]),
-    );
+            .allow_methods(vec![Method::GET]))
+        .layer(Extension(pool));
 
     // app
     let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
@@ -37,4 +48,21 @@ async fn main() {
 
 async fn handler() -> &'static str {
     ":q!"
+}
+
+async fn connection_pool_extractor(
+    Extension(pool): Extension<PgPool>
+) -> Result<String, (StatusCode, String)> {
+    sqlx::query_scalar("SELECT 'hello world from pg!'")
+        .fetch_one(&pool)
+        .await
+        .map_err(internal_error)
+}
+
+// Utility function for mapping errors into 500 http response
+fn internal_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
